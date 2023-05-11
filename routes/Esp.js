e3ff4df104fdd86590b13c9c5d7461b4bed12917
MinhThang <Thanmn9403@gmail.com>
2023-05-11 18:57:14 +0700

    11/5

--- routes/Esp.js
+++ routes/Esp.js
@@ -4,7 +4,7 @@ const espController = require("../controllers/espController")
 // router.get("/", espController.getField)
 router.get(`/`, espController.getUserField)
 router.get("/:pid", espController.getAField)
-router.post("/:pid", espController.addFeed)
+router.post("/addFeed", espController.addFeed)
 router.post("/", espController.addField)
 router.delete("/:pid", espController.delField)
 
