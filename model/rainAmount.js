ee5d951945fae6e84c8d16b8e338afc19a9e81e0
MinhThang <Thanmn9403@gmail.com>
2023-07-31 15:17:48 +0700

    add rain unit

--- model/rainAmount.js
+++ model/rainAmount.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
 const rainSChema = new mongoose.Schema({
-    uid: String,
-    pid:String,
-    espName:String,
-    created_at: { type: Date, default: Date.now },
-    feeds:[
-        {
-            generated_date: String,
-            generated_time: String,
-            entryId: Number,
-            field1: String
-        }
-    ]
-})
-const rain = mongoose.model('rain', rainSChema);
-module.exports = rain 
+  uid: String,
+  pid: String,
+  espName: String,
+  created_at: { type: Date, default: Date.now },
+  feeds: [
+    {
+      generated_date: String,
+      generated_time: String,
+      unit: String,
+      entryId: Number,
+      field1: String,
+    },
+  ],
+});
+const rain = mongoose.model("rain", rainSChema);
+module.exports = rain;
